376d674db15aadffa678a187ea79fd1adbf62753
Marosán Gergő <marosangergo1@gmail.com>
2020-12-10 22:43:30 +0100

    Add project files.

---
+++ DarwinUI/MainWindow.xaml.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Windows;
+using System.Windows.Input;
+
+namespace DarwinUI
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+
+        private void drag(object sender, MouseButtonEventArgs e)
+        {
+            try
+            {
+                DragMove();
+            }
+            catch (Exception)
+            {
+                //throw;
+            }
+        }
+
+        private void exitwin(object sender, MouseButtonEventArgs e)
+        {
+            this.Close();
+        }
+
+        private void minimisewin(object sender, MouseButtonEventArgs e)
+        {
+            this.WindowState = WindowState.Minimized;
+        }
+    }
+}
